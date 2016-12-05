93e9ad06ee5186b57c4a59a5fcaa870fe60d2ec1
Robert Rüger <mail@robertrueger.de>
2016-12-05 23:42:56 +0100

    pretty much finished the stdVector iterator

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-gfortran stdVectorInt.F90 stdVectorReal.F90 test.F90 -o test
+gfortran -g stdVectorInt.F90 stdVectorReal.F90 test.F90 -o test
--- test.F90
+++ test.F90
@@ -6,10 +6,15 @@ program stdVectorTest
    implicit none
 
    type(stdVectorInt) :: a, b
+   type(stdVectorIntIterator) :: i,j
 
    call a%New()
    a = [5,5,76,3]
    call a%Insert(2, [1,1,1])
    call a%Print()
+
+   i = a%Begin() + 4
+   write (*,*) i%value
+
 
 end program
