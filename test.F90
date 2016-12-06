c6d3d74b3554887e72540d51876b53777f77920e
Robert Rüger <mail@robertrueger.de>
2016-12-06 18:46:56 +0100

    started adding the public ptr to stdVector

--- test.F90
+++ test.F90
@@ -12,6 +12,7 @@ program stdVectorTest
    a = [5,5,76,3]
    call a%Insert(2, [1,1,1])
    call a%Print()
+   write (*,*) 'sizes = ', a%size, size(a%data), size(a%ptr)
 
    i = a%Begin() + 4
    write (*,*) i%value
