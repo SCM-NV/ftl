464535ebbedac69db44fecaaaf6bbe45eb904910
Robert Rüger <mail@robertrueger.de>
2016-12-22 22:16:11 +0100

    comparator function for ftlSort

--- tests/ftlSortTests.F90
+++ tests/ftlSortTests.F90
@@ -29,12 +29,20 @@ contains
       type(ftlVectorInt) :: v
 
       call v%New([9,6,3,4,7])
+
       call ftlSort(v)
 
       ASSERT(v%Size() == 5)
       ASSERT(v%front == 3)
       ASSERT(v%back == 9)
       ASSERT(all(v%data == [3,4,6,7,9]))
+
+      call ftlSort(v, Greater)
+
+      ASSERT(v%Size() == 5)
+      ASSERT(v%front == 9)
+      ASSERT(v%back == 3)
+      ASSERT(all(v%data == [9,7,6,4,3]))
 
    end subroutine
 
@@ -44,6 +52,7 @@ contains
       type(ftlListIntIterator) :: it
 
       call l%New([9,6,3,4,7])
+
       call ftlSort(l)
 
       ASSERT(l%Size() == 5)
@@ -63,7 +72,32 @@ contains
       call it%Inc()
       ASSERT(it == l%End())
 
+      call ftlSort(l, Greater)
+
+      ASSERT(l%Size() == 5)
+      ASSERT(l%front == 9)
+      ASSERT(l%back == 3)
+
+      it = l%Begin()
+      ASSERT(it%value == 9)
+      call it%Inc()
+      ASSERT(it%value == 7)
+      call it%Inc()
+      ASSERT(it%value == 6)
+      call it%Inc()
+      ASSERT(it%value == 4)
+      call it%Inc()
+      ASSERT(it%value == 3)
+      call it%Inc()
+      ASSERT(it == l%End())
+
    end subroutine
+
+
+   pure logical function Greater(n,m)
+      integer, intent(in) :: n,m
+      Greater = (n > m)
+   end function
 
 
 end module
--- tests/instantiations/ftlSortftlVectorInt.F90
+++ tests/instantiations/ftlSortftlVectorInt.F90
@@ -1,5 +1,6 @@
 #define FTL_TEMPLATE_TYPE integer
 #define FTL_TEMPLATE_TYPE_NAME Int
+#define FTL_TEMPLATE_TYPE_PROVIDES_COMPARATOR_SMALLER
 #include "ftlVector.F90_template"
 #define FTL_INSTANTIATE_TEMPLATE
 #include "ftlSort.F90_template"
