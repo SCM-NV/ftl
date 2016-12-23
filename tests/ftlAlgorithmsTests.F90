1c59136fac949ba028e2d982e24ad90b67164760
Robert Rüger <mail@robertrueger.de>
2016-12-23 01:13:00 +0100

    ftlAlgorithms: implemented some non-modifying sequence operations

--- tests/ftlAlgorithmsTests.F90
+++ tests/ftlAlgorithmsTests.F90
@@ -19,8 +19,107 @@ contains
 
       write (*,'(A)') 'Running ftlAlgorithms tests ...'
 
+      call testAllOf
+      call testAnyOf
+      call testNoneOf
+
+      call testCount
+      call testCountIf
+
       call testSortVector
       call testSortList
+
+   end subroutine
+
+
+   subroutine testAllOf
+      type(ftlVectorInt) :: v
+      type(ftlVectorIntIterator) :: it
+
+      call v%New()
+      ASSERT(ftlAllOf(v,IsEven))
+
+      call v%New([2,4,6,8,10,12,13])
+      ASSERT(.not.ftlAllOf(v,IsEven))
+
+      it = v%End() - 1
+      ASSERT(ftlAllOf(v%Begin(),it,IsEven))
+
+   end subroutine
+
+
+   subroutine testAnyOf
+      type(ftlVectorInt) :: v
+      type(ftlVectorIntIterator) :: it
+
+      call v%New()
+      ASSERT(.not.ftlAnyOf(v,IsEven))
+
+      call v%New([2,4,6,8,10,12,13])
+      ASSERT(ftlAnyOf(v,IsUneven))
+
+      it = v%End() - 1
+      ASSERT(.not.ftlAnyOf(v%Begin(),it,IsUneven))
+
+   end subroutine
+
+
+   subroutine testNoneOf
+      type(ftlListInt) :: l
+      type(ftlListIntIterator) :: it
+
+      call l%New()
+      ASSERT(ftlNoneOf(l,IsEven))
+
+      call l%New([2,4,6,8,10,12,13])
+      ASSERT(.not.ftlNoneOf(l,IsUneven))
+
+      it = l%End()
+      call it%Dec()
+      call it%Dec()
+      ASSERT(ftlNoneOf(l%Begin(),it,IsUneven))
+
+   end subroutine
+
+
+   subroutine testCount
+      type(ftlVectorInt) :: v
+      type(ftlVectorIntIterator) :: it
+
+      call v%New([6,2,895,6,3,6,43,2,6,3])
+
+      ASSERT(ftlCount(v,6) == 4)
+      ASSERT(ftlCount(v,2) == 2)
+      ASSERT(ftlCount(v,895) == 1)
+      ASSERT(ftlCount(v,3) == 2)
+      ASSERT(ftlCount(v,43) == 1)
+
+      it = v%End() - 2
+
+      ASSERT(ftlCount(v%Begin(),it,6) == 3)
+      ASSERT(ftlCount(v%Begin(),it,2) == 2)
+      ASSERT(ftlCount(v%Begin(),it,895) == 1)
+      ASSERT(ftlCount(v%Begin(),it,3) == 1)
+      ASSERT(ftlCount(v%Begin(),it,43) == 1)
+
+   end subroutine
+
+
+   subroutine testCountIf
+      type(ftlListInt) :: l
+      type(ftlListIntIterator) :: it
+
+      call l%New([6,2,895,6,3,6,43,2,6,3])
+
+      ASSERT(ftlCountIf(l,IsEven) == 6)
+      ASSERT(ftlCountIf(l,IsUneven) == 4)
+
+      it = l%End()
+      call it%Dec()
+      call it%Dec()
+
+      ASSERT(ftlCountIf(l%Begin(),it,IsEven) == 5)
+      ASSERT(ftlCountIf(l%Begin(),it,IsUneven) == 3)
 
    end subroutine
 
@@ -93,6 +192,20 @@ contains
 
    end subroutine
 
+
+   ! example unary predicates:
+
+   pure logical function IsEven(n)
+      integer, intent(in) :: n
+      IsEven = (mod(n,2) == 0)
+   end function
+
+   pure logical function IsUneven(n)
+      integer, intent(in) :: n
+      IsUneven = (mod(n,2) == 1)
+   end function
+
+   ! example comparator:
 
    pure logical function Greater(n,m)
       integer, intent(in) :: n,m
