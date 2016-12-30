0109c2e5c904bd76bf7195ae0a1759cd77fbbc3a
Robert Rüger <mail@robertrueger.de>
2016-12-30 02:22:27 +0100

    defined operator== for my example BigType (real problem needs to addressed though)

--- instantiations/derived_types/BigType.F90
+++ instantiations/derived_types/BigType.F90
@@ -9,4 +9,20 @@ module BigTypeModule
       character(len=10000) :: someChars
    end type
 
+   public :: operator(==)
+   interface operator(==)
+      module procedure EqualOther
+   end interface
+
+contains
+
+   pure logical function EqualOther(self, other) result(equal)
+      type(BigType), intent(in) :: self, other
+
+      equal = all(self%someInts == other%someInts)   .and. \
+              all(self%someReals == other%someReals) .and. \
+              self%someChars == other%someChars
+
+   end function
+
 end module
