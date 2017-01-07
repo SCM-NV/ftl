b441bc24d232e67e925fdab6e3de04840a560715
Robert Rüger <mail@robertrueger.de>
2017-01-07 20:44:57 +0100

    implemented ftlHashMap find and erase

--- tests/ftlHashMapTests.F90
+++ tests/ftlHashMapTests.F90
@@ -38,9 +38,11 @@ contains
 
       call testNewDefault
       call testSetAndGet
-      call testErase
       call testRehash
       call testIterators
+      call testFind
+      call testEraseKey
+      call testEraseIterators
 
    end subroutine
 
@@ -162,78 +164,6 @@ contains
    end subroutine
 
 
-   subroutine testErase
-      type(ftlHashMapStrInt) :: um
-
-      call um%New(10)
-
-      ASSERT(um%Empty())
-      ASSERT(size(um) == 0)
-
-      call um%Set('bar ',  1)
-      call um%Set('test',  2)
-      call um%Set('blub',  3)
-      call um%Set('jipi',  4)
-      call um%Set('fort',  5)
-      call um%Set('ran ',  6)
-      call um%Set('is m',  7)
-      call um%Set('y fa',  8)
-      call um%Set('vour',  9)
-      call um%Set('ite ', 10)
-      call um%Set('lang', 11)
-      call um%Set('not ', 12)
-      call um%Set('rly!', 13)
-
-      ASSERT(size(um) == 13)
-
-      call um%Erase('bar ')
-      ASSERT(.not.um%Has('bar '))
-      ASSERT(size(um) == 12)
-
-      ! test double erase
-
-      call um%Erase('bar ')
-      ASSERT(.not.um%Has('bar '))
-      ASSERT(size(um) == 12)
-
-      ! erase all the rest
-
-      call um%Erase('test')
-      call um%Erase('blub')
-      call um%Erase('jipi')
-      call um%Erase('fort')
-      call um%Erase('ran ')
-      call um%Erase('is m')
-      call um%Erase('y fa')
-      call um%Erase('vour')
-      call um%Erase('ite ')
-      call um%Erase('lang')
-      call um%Erase('not ')
-      call um%Erase('rly!')
-
-      ASSERT(.not.um%Has('test'))
-      ASSERT(.not.um%Has('blub'))
-      ASSERT(.not.um%Has('jipi'))
-      ASSERT(.not.um%Has('fort'))
-      ASSERT(.not.um%Has('ran '))
-      ASSERT(.not.um%Has('is m'))
-      ASSERT(.not.um%Has('y fa'))
-      ASSERT(.not.um%Has('vour'))
-      ASSERT(.not.um%Has('ite '))
-      ASSERT(.not.um%Has('lang'))
-      ASSERT(.not.um%Has('not '))
-      ASSERT(.not.um%Has('rly!'))
-
-      ASSERT(um%Empty())
-      ASSERT(size(um) == 0)
-
-#ifdef FTL_NO_FINALIZERS
-      call um%Delete()
-#endif
-
-   end subroutine
-
-
    subroutine testRehash
       type(ftlHashMapStrInt) :: um
 
@@ -309,6 +239,169 @@ contains
       enddo
 
       ASSERT(it == um%End())
+
+#ifdef FTL_NO_FINALIZERS
+      call um%Delete()
+#endif
+
+   end subroutine
+
+
+   subroutine testFind
+      type(ftlHashMapStrInt) :: um
+
+      call um%New(10)
+
+      call um%Set('test',  0)
+      call um%Set('blub',  1)
+      call um%Set('jipi',  2)
+      call um%Set('fort',  3)
+      call um%Set('ran ',  4)
+      call um%Set('is m',  5)
+      call um%Set('y fa',  6)
+      call um%Set('vour',  7)
+      call um%Set('ite ',  8)
+      call um%Set('lang',  9)
+      call um%Set('not ', 10)
+      call um%Set('rly!', 11)
+
+      call um%Rehash(10) ! make sure we have overfull buckets
+
+      ASSERT(um%Find('test') /= um%End())
+      ASSERT(um%Find('blub') /= um%End())
+      ASSERT(um%Find('jipi') /= um%End())
+      ASSERT(um%Find('fort') /= um%End())
+      ASSERT(um%Find('ran ') /= um%End())
+      ASSERT(um%Find('is m') /= um%End())
+      ASSERT(um%Find('y fa') /= um%End())
+      ASSERT(um%Find('vour') /= um%End())
+      ASSERT(um%Find('ite ') /= um%End())
+      ASSERT(um%Find('lang') /= um%End())
+      ASSERT(um%Find('not ') /= um%End())
+      ASSERT(um%Find('rly!') /= um%End())
+
+      ASSERT(um%Find('----') == um%End())
+
+#ifdef FTL_NO_FINALIZERS
+      call um%Delete()
+#endif
+
+   end subroutine
+
+
+   subroutine testEraseKey
+      type(ftlHashMapStrInt) :: um
+
+      call um%New(10)
+
+      ASSERT(um%Empty())
+      ASSERT(size(um) == 0)
+
+      call um%Set('bar ',  1)
+      call um%Set('test',  2)
+      call um%Set('blub',  3)
+      call um%Set('jipi',  4)
+      call um%Set('fort',  5)
+      call um%Set('ran ',  6)
+      call um%Set('is m',  7)
+      call um%Set('y fa',  8)
+      call um%Set('vour',  9)
+      call um%Set('ite ', 10)
+      call um%Set('lang', 11)
+      call um%Set('not ', 12)
+      call um%Set('rly!', 13)
+
+      ASSERT(size(um) == 13)
+
+      call um%Erase('bar ')
+      ASSERT(.not.um%Has('bar '))
+      ASSERT(size(um) == 12)
+
+      ! test double erase
+
+      call um%Erase('bar ')
+      ASSERT(.not.um%Has('bar '))
+      ASSERT(size(um) == 12)
+
+      ! erase all the rest
+
+      call um%Erase('test')
+      call um%Erase('blub')
+      call um%Erase('jipi')
+      call um%Erase('fort')
+      call um%Erase('ran ')
+      call um%Erase('is m')
+      call um%Erase('y fa')
+      call um%Erase('vour')
+      call um%Erase('ite ')
+      call um%Erase('lang')
+      call um%Erase('not ')
+      call um%Erase('rly!')
+
+      ASSERT(.not.um%Has('test'))
+      ASSERT(.not.um%Has('blub'))
+      ASSERT(.not.um%Has('jipi'))
+      ASSERT(.not.um%Has('fort'))
+      ASSERT(.not.um%Has('ran '))
+      ASSERT(.not.um%Has('is m'))
+      ASSERT(.not.um%Has('y fa'))
+      ASSERT(.not.um%Has('vour'))
+      ASSERT(.not.um%Has('ite '))
+      ASSERT(.not.um%Has('lang'))
+      ASSERT(.not.um%Has('not '))
+      ASSERT(.not.um%Has('rly!'))
+
+      ASSERT(um%Empty())
+      ASSERT(size(um) == 0)
+
+#ifdef FTL_NO_FINALIZERS
+      call um%Delete()
+#endif
+
+   end subroutine
+
+
+   subroutine testEraseIterators
+      type(ftlHashMapStrInt) :: um
+      type(ftlHashMapStrIntIterator) :: it
+
+      call um%New(10)
+
+      call um%Set('test',  0)
+      call um%Set('blub',  1)
+      call um%Set('jipi',  2)
+      call um%Set('fort',  3)
+      call um%Set('ran ',  4)
+      call um%Set('is m',  5)
+      call um%Set('y fa',  6)
+      call um%Set('vour',  7)
+      call um%Set('ite ',  8)
+      call um%Set('lang',  9)
+      call um%Set('not ', 10)
+      call um%Set('rly!', 11)
+
+      call um%Rehash(10) ! make sure we have overfull buckets
+
+      ASSERT(size(um) == 12)
+
+      ASSERT(um%Has('test'))
+      call um%Erase('test')
+      ASSERT(.not.um%Has('test'))
+
+      ASSERT(size(um) == 11)
+
+      ASSERT(um%Has('fort'))
+      call um%Erase(um%Find('fort'))
+      ASSERT(.not.um%Has('fort'))
+
+      ASSERT(size(um) == 10)
+
+      it = um%Begin()
+      call it%Inc()
+      call it%Inc()
+      call um%Erase(it, um%End())
+
+      ASSERT(size(um) == 2)
 
 #ifdef FTL_NO_FINALIZERS
       call um%Delete()
